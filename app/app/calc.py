544665c8a3e5f4cbdf430092236121e710e0cc64
ruzica4 <ruzicagagic4@gmail.com>
2023-06-04 18:48:42 +0200

    Added db service to docker-compose file, added missing dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """
+    Add x and y and return results
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtract x from y and return result.
+    """
+    return y - x
