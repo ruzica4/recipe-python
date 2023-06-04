208eb7cdf747bc9506962b57608be2b79193248c
ruzica4 <ruzicagagic4@gmail.com>
2023-06-04 19:08:36 +0200

    Code formatting improved try 2

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,6 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-
     """
     Test the calc module.
     """
@@ -17,14 +16,12 @@ class CalcTests(SimpleTestCase):
         """
         Test adding numbers together.
         """
-
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """
-        Test subtracking numbers.
+        Test subtracting numbers.
         """
-
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
